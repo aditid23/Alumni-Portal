62a45e1ca2061d1ea139848592757c0d51afc7ef
Harshini070 <sree07harshini@gmail.com>
2024-08-18 16:27:44 +0530

    Add files via upload

---
+++ AluminiPortal/Alumni-Portal/Alumni-Portal/calc/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'home.html')
+
+def newpost(request):
+    return render(request, 'newpost.html')
+
+def create(request):
+    para = request.POST.get('paragraph', '')  
+    return render(request, 'home.html', {'para': para})
